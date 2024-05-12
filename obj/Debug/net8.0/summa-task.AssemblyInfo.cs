8ec2f47dceadcc11dbf9c396e3bffc1b0e3a4ec6
Alon <alonkendler@gmail.com>
2024-05-12 19:52:31 +0300

    attempt to sue different tesserac lib"

--- obj/Debug/net8.0/summa-task.AssemblyInfo.cs
+++ obj/Debug/net8.0/summa-task.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("summa-task")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+631ebe6d1286fc0c962064a126c2cd50b797acca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be1add3b8e4d934ce356483f607d1be16847a4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("summa-task")]
 [assembly: System.Reflection.AssemblyTitleAttribute("summa-task")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
