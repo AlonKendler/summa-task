6d3e43781316cfd61ef9f73697def1984225f79a
Alon <alonkendler@gmail.com>
2024-05-15 14:42:22 +0300

    remove three.js

--- obj/Debug/net8.0/summa-task.AssemblyInfo.cs
+++ obj/Debug/net8.0/summa-task.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("summa-task")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afa33eb12f729061263b68e4f9a4787a686e46b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36b7b4d849c3a2be6c601b75529cc8194c607eb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("summa-task")]
 [assembly: System.Reflection.AssemblyTitleAttribute("summa-task")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
